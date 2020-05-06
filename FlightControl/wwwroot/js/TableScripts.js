9a9502fe9b6d5c6b49cbef38797b8b0b990dc7d7
Yoel Sherwin <yoel.sherwin@gmail.co.il>
2020-05-06 19:50:50 +0300

    working on drag and drop

--- FlightControl/wwwroot/js/TableScripts.js
+++ FlightControl/wwwroot/js/TableScripts.js
@@ -1,34 +1,14 @@
 // Empty JS for your own code to be here
 function onDrop(ev) {
+    ev.preventDefault();
     document.getElementById("details").innerHTML = "drop";
     document.getElementById("dragAndDrop").style.display = "none";
     $("#dragArea").show();
-    ev.preventDefault();
     if (ev.dataTransfer.items[0].kind === 'file') {
         let file = ev.dataTransfer.items[0].getAsFile();
         document.getElementById("details").innerHTML = file.name;
         let flightURL = "../api/FlightPlan";
     }
-    /*console.log('File(s) dropped');
-
-    // Prevent default behavior (Prevent file from being opened)
-    ev.preventDefault();
-
-    if (ev.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-            // If dropped items aren't files, reject them
-            if (ev.dataTransfer.items[i].kind === 'file') {
-                var file = ev.dataTransfer.items[i].getAsFile();
-                console.log('... file[' + i + '].name = ' + file.name);
-            }
-        }
-    } else {
-        // Use DataTransfer interface to access the file(s)
-        for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-            console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-        }
-    }*/
 }
 
 function onDragOver(ev) {
